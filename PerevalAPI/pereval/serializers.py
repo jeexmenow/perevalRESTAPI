d358b97fa2fca9f06a83e300cf5b7cf5f5bcba90
jeexy <my_karmen@mail.ru>
2024-04-06 01:09:09 +0300

    Методы. GET /submitData/<id> и PATCH/submitData/<id>

--- PerevalAPI/pereval/serializers.py
+++ PerevalAPI/pereval/serializers.py
@@ -29,39 +29,53 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class PerevalSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    coords = CoordSerializer()
-    level = LevelSerializer()
+    user_id = UserSerializer()
+    coords_id = CoordSerializer()
+    level_diff = LevelSerializer(allow_null=True)
     images = ImagesSerializer(many=True)
 
     class Meta:
         model = Pereval
-        fields = ['beauty_title', 'title', 'other_titles', 'connect', 'add_time',
-                  'user', 'coords', 'level', 'images']
+        fields = ['id', 'user_id', 'coords_id', 'level_diff', 'beauty_title', 'title', 'other_titles', 'connect',
+                  'add_time', 'image',
+                  'status']
 
-    def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        coords_data = validated_data.pop('coords')
-        level_data = validated_data.pop('level')
-        images_data = validated_data.pop('images')
+        # restricts altering user's data while updating pereval
+        def validate(self, data):
+            if self.instance is not None:
+                user_instance = self.instance.user_id
+                user_data = data.get('user_id')
+                valid_user_fields = [
+                    user_instance.email != user_data['email'],
+                    user_instance.full_name != user_data['full_name'],
+                    user_instance.phone != user_data['phone']
+                ]
+                if user_data and any(valid_user_fields):
+                    raise serializers.ValidationError({'cant be changed'})
+            return data
 
-        pass_user = MyUser.objects.filter(email=user_data['email'])
+    # def create(self, validated_data):
+    # user_data = validated_data.pop('user')
+    # coords_data = validated_data.pop('coords')
+    # level_data = validated_data.pop('level')
+    # images_data = validated_data.pop('images')
 
-        if pass_user.exists():
-            user_ser = UserSerializer(data=user_data)
-            user = user_ser.save()
-        else:
-            user = MyUser.objects.create(**user_data)
+    #  pass_user = MyUser.objects.filter(email=user_data['email'])
 
-        coords = Coord.objects.create(**coords_data)
-        level = Level.objects.create(**level_data)
+    #  if pass_user.exists():
+    #     user_ser = UserSerializer(data=user_data)
+    #     user = user_ser.save()
+    # else:
+    #     user = MyUser.objects.create(**user_data)
 
-        pereval = Pereval.objects.create(user=user, coords=coords, level=level, **validated_data)
+    # coords = Coord.objects.create(**coords_data)
+    #  level = Level.objects.create(**level_data)
 
-        for i in images_data:
-            data = i.pop('data')
-            title = i.pop('title')
-            Images.objects.create(data=data, pereval=pereval, title=title)
+    #  pereval = Pereval.objects.create(user=user, coords=coords, level=level, **validated_data)
 
-        return pereval
+    #  for i in images_data:
+    #     data = i.pop('data')
+    #      title = i.pop('title')
+    #      Images.objects.create(data=data, pereval=pereval, title=title)
 
+    #  return pereval
